33f8a843d28bebe2d55d5502087a38fa605951f0
AlekseyBorichevskiy <borichevskiy03@mail.ru>
2021-07-15 12:16:43 +0300

    g

--- generator.go
+++ generator.go
@@ -9,27 +9,29 @@ import (
 
 // length- quantity of operations in expression
 func Generate(length uint) string {
+	const (
+		operands  = "0123456789"
+		operators = "+-"
+	)
 	rand.Seed(time.Now().UnixNano())
 	var builder = strings.Builder{}
 
-	var generate = func(str string) string {
+	var any = func(str string) string {
 		return string(str[rand.Intn(len(str))])
 	}
-	var isSpace = func() string {
+	var space = func() string {
 		if rand.Intn(2) == 0 {
 			return ""
 		}
 		return " "
 	}
-	var edger = func(str string) { fmt.Fprint(&builder, isSpace(), str, isSpace()) }
+	var edger = func(str string) { fmt.Fprint(&builder, space(), str, space()) }
 
 	var operand = func() {
-		var operands = "0123456789"
-		edger(generate(operands))
+		edger(any(operands))
 	}
 	var operator = func() {
-		var operators = "+-"
-		edger(generate(operators))
+		edger(any(operators))
 	}
 	operand()
 
